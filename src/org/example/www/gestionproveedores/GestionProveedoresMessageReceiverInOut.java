3f862f76829363043de71299f33cd1deaede78c7
Leonardo Rodríguez Martínez <Leonardo Rodrguez Martnez>
2017-05-17 12:52:32 +0200

    añadida operación GenerarOferta

---
+++ src/org/example/www/gestionproveedores/GestionProveedoresMessageReceiverInOut.java
@@ -0,0 +1,266 @@
+
+/**
+ * GestionProveedoresMessageReceiverInOut.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
+ */
+        package org.example.www.gestionproveedores;
+
+        /**
+        *  GestionProveedoresMessageReceiverInOut message receiver
+        */
+
+        public class GestionProveedoresMessageReceiverInOut extends org.apache.axis2.receivers.AbstractInOutMessageReceiver{
+
+
+        public void invokeBusinessLogic(org.apache.axis2.context.MessageContext msgContext, org.apache.axis2.context.MessageContext newMsgContext)
+        throws org.apache.axis2.AxisFault{
+
+        try {
+
+        // get the implementation class for the Web Service
+        Object obj = getTheImplementationObject(msgContext);
+
+        GestionProveedoresSkeleton skel = (GestionProveedoresSkeleton)obj;
+        //Out Envelop
+        org.apache.axiom.soap.SOAPEnvelope envelope = null;
+        //Find the axisOperation that has been set by the Dispatch phase.
+        org.apache.axis2.description.AxisOperation op = msgContext.getOperationContext().getAxisOperation();
+        if (op == null) {
+        throw new org.apache.axis2.AxisFault("Operation is not located, if this is doclit style the SOAP-ACTION should specified via the SOAP Action to use the RawXMLProvider");
+        }
+
+        java.lang.String methodName;
+        if((op.getName() != null) && ((methodName = org.apache.axis2.util.JavaUtils.xmlNameToJavaIdentifier(op.getName().getLocalPart())) != null)){
+
+
+        
+
+            if("generarOferta".equals(methodName)){
+                
+                org.example.www.gestionproveedores.GenerarOfertaResponse generarOfertaResponse9 = null;
+	                        org.example.www.gestionproveedores.GenerarOferta wrappedParam =
+                                                             (org.example.www.gestionproveedores.GenerarOferta)fromOM(
+                                    msgContext.getEnvelope().getBody().getFirstElement(),
+                                    org.example.www.gestionproveedores.GenerarOferta.class,
+                                    getEnvelopeNamespaces(msgContext.getEnvelope()));
+                                                
+                                               generarOfertaResponse9 =
+                                                   
+                                                   
+                                                         skel.generarOferta(wrappedParam)
+                                                    ;
+                                            
+                                        envelope = toEnvelope(getSOAPFactory(msgContext), generarOfertaResponse9, false, new javax.xml.namespace.QName("http://www.example.org/GestionProveedores/",
+                                                    "generarOferta"));
+                                    } else 
+
+            if("leerOfertas".equals(methodName)){
+                
+                org.example.www.gestionproveedores.LeerOfertasResponse leerOfertasResponse11 = null;
+	                        org.example.www.gestionproveedores.LeerOfertas wrappedParam =
+                                                             (org.example.www.gestionproveedores.LeerOfertas)fromOM(
+                                    msgContext.getEnvelope().getBody().getFirstElement(),
+                                    org.example.www.gestionproveedores.LeerOfertas.class,
+                                    getEnvelopeNamespaces(msgContext.getEnvelope()));
+                                                
+                                               leerOfertasResponse11 =
+                                                   
+                                                   
+                                                         skel.leerOfertas(wrappedParam)
+                                                    ;
+                                            
+                                        envelope = toEnvelope(getSOAPFactory(msgContext), leerOfertasResponse11, false, new javax.xml.namespace.QName("http://www.example.org/GestionProveedores/",
+                                                    "leerOfertas"));
+                                    
+            } else {
+              throw new java.lang.RuntimeException("method not found");
+            }
+        
+
+        newMsgContext.setEnvelope(envelope);
+        }
+        }
+        catch (java.lang.Exception e) {
+        throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+        }
+        
+        //
+            private  org.apache.axiom.om.OMElement  toOM(org.example.www.gestionproveedores.GenerarOferta param, boolean optimizeContent)
+            throws org.apache.axis2.AxisFault {
+
+            
+                        try{
+                             return param.getOMElement(org.example.www.gestionproveedores.GenerarOferta.MY_QNAME,
+                                          org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+                        } catch(org.apache.axis2.databinding.ADBException e){
+                            throw org.apache.axis2.AxisFault.makeFault(e);
+                        }
+                    
+
+            }
+        
+            private  org.apache.axiom.om.OMElement  toOM(org.example.www.gestionproveedores.GenerarOfertaResponse param, boolean optimizeContent)
+            throws org.apache.axis2.AxisFault {
+
+            
+                        try{
+                             return param.getOMElement(org.example.www.gestionproveedores.GenerarOfertaResponse.MY_QNAME,
+                                          org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+                        } catch(org.apache.axis2.databinding.ADBException e){
+                            throw org.apache.axis2.AxisFault.makeFault(e);
+                        }
+                    
+
+            }
+        
+            private  org.apache.axiom.om.OMElement  toOM(org.example.www.gestionproveedores.LeerOfertas param, boolean optimizeContent)
+            throws org.apache.axis2.AxisFault {
+
+            
+                        try{
+                             return param.getOMElement(org.example.www.gestionproveedores.LeerOfertas.MY_QNAME,
+                                          org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+                        } catch(org.apache.axis2.databinding.ADBException e){
+                            throw org.apache.axis2.AxisFault.makeFault(e);
+                        }
+                    
+
+            }
+        
+            private  org.apache.axiom.om.OMElement  toOM(org.example.www.gestionproveedores.LeerOfertasResponse param, boolean optimizeContent)
+            throws org.apache.axis2.AxisFault {
+
+            
+                        try{
+                             return param.getOMElement(org.example.www.gestionproveedores.LeerOfertasResponse.MY_QNAME,
+                                          org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+                        } catch(org.apache.axis2.databinding.ADBException e){
+                            throw org.apache.axis2.AxisFault.makeFault(e);
+                        }
+                    
+
+            }
+        
+                    private  org.apache.axiom.soap.SOAPEnvelope toEnvelope(org.apache.axiom.soap.SOAPFactory factory, org.example.www.gestionproveedores.GenerarOfertaResponse param, boolean optimizeContent, javax.xml.namespace.QName methodQName)
+                        throws org.apache.axis2.AxisFault{
+                      try{
+                          org.apache.axiom.soap.SOAPEnvelope emptyEnvelope = factory.getDefaultEnvelope();
+                           
+                                    emptyEnvelope.getBody().addChild(param.getOMElement(org.example.www.gestionproveedores.GenerarOfertaResponse.MY_QNAME,factory));
+                                
+
+                         return emptyEnvelope;
+                    } catch(org.apache.axis2.databinding.ADBException e){
+                        throw org.apache.axis2.AxisFault.makeFault(e);
+                    }
+                    }
+                    
+                         private org.example.www.gestionproveedores.GenerarOfertaResponse wrapGenerarOferta(){
+                                org.example.www.gestionproveedores.GenerarOfertaResponse wrappedElement = new org.example.www.gestionproveedores.GenerarOfertaResponse();
+                                return wrappedElement;
+                         }
+                    
+                    private  org.apache.axiom.soap.SOAPEnvelope toEnvelope(org.apache.axiom.soap.SOAPFactory factory, org.example.www.gestionproveedores.LeerOfertasResponse param, boolean optimizeContent, javax.xml.namespace.QName methodQName)
+                        throws org.apache.axis2.AxisFault{
+                      try{
+                          org.apache.axiom.soap.SOAPEnvelope emptyEnvelope = factory.getDefaultEnvelope();
+                           
+                                    emptyEnvelope.getBody().addChild(param.getOMElement(org.example.www.gestionproveedores.LeerOfertasResponse.MY_QNAME,factory));
+                                
+
+                         return emptyEnvelope;
+                    } catch(org.apache.axis2.databinding.ADBException e){
+                        throw org.apache.axis2.AxisFault.makeFault(e);
+                    }
+                    }
+                    
+                         private org.example.www.gestionproveedores.LeerOfertasResponse wrapLeerOfertas(){
+                                org.example.www.gestionproveedores.LeerOfertasResponse wrappedElement = new org.example.www.gestionproveedores.LeerOfertasResponse();
+                                return wrappedElement;
+                         }
+                    
+
+
+        /**
+        *  get the default envelope
+        */
+        private org.apache.axiom.soap.SOAPEnvelope toEnvelope(org.apache.axiom.soap.SOAPFactory factory){
+        return factory.getDefaultEnvelope();
+        }
+
+
+        private  java.lang.Object fromOM(
+        org.apache.axiom.om.OMElement param,
+        java.lang.Class type,
+        java.util.Map extraNamespaces) throws org.apache.axis2.AxisFault{
+
+        try {
+        
+                if (org.example.www.gestionproveedores.GenerarOferta.class.equals(type)){
+                
+                        return org.example.www.gestionproveedores.GenerarOferta.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+                    
+
+                }
+            
+                if (org.example.www.gestionproveedores.GenerarOfertaResponse.class.equals(type)){
+                
+                        return org.example.www.gestionproveedores.GenerarOfertaResponse.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+                    
+
+                }
+            
+                if (org.example.www.gestionproveedores.LeerOfertas.class.equals(type)){
+                
+                        return org.example.www.gestionproveedores.LeerOfertas.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+                    
+
+                }
+            
+                if (org.example.www.gestionproveedores.LeerOfertasResponse.class.equals(type)){
+                
+                        return org.example.www.gestionproveedores.LeerOfertasResponse.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+                    
+
+                }
+            
+        } catch (java.lang.Exception e) {
+        throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+           return null;
+        }
+
+
+
+    
+
+        /**
+        *  A utility method that copies the namepaces from the SOAPEnvelope
+        */
+        private java.util.Map getEnvelopeNamespaces(org.apache.axiom.soap.SOAPEnvelope env){
+        java.util.Map returnMap = new java.util.HashMap();
+        java.util.Iterator namespaceIterator = env.getAllDeclaredNamespaces();
+        while (namespaceIterator.hasNext()) {
+        org.apache.axiom.om.OMNamespace ns = (org.apache.axiom.om.OMNamespace) namespaceIterator.next();
+        returnMap.put(ns.getPrefix(),ns.getNamespaceURI());
+        }
+        return returnMap;
+        }
+
+        private org.apache.axis2.AxisFault createAxisFault(java.lang.Exception e) {
+        org.apache.axis2.AxisFault f;
+        Throwable cause = e.getCause();
+        if (cause != null) {
+            f = new org.apache.axis2.AxisFault(e.getMessage(), cause);
+        } else {
+            f = new org.apache.axis2.AxisFault(e.getMessage());
+        }
+
+        return f;
+    }
+
+        }//end of class
+    
---
+++ src/org/example/www/gestionproveedores/GestionProveedoresSkeleton.java
@@ -0,0 +1,216 @@
+
+/**
+ * GestionProveedoresSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
+ */
+package org.example.www.gestionproveedores;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import java.sql.*;
+/**
+ * GestionProveedoresSkeleton java skeleton for the axisService
+ */
+public class GestionProveedoresSkeleton {
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param generarOferta
+	 * @return generarOfertaResponse
+	 */
+
+	public org.example.www.gestionproveedores.GenerarOfertaResponse generarOferta(
+			org.example.www.gestionproveedores.GenerarOferta generarOferta) {
+	
+		GenerarOfertaResponse respuesta = new GenerarOfertaResponse();
+		
+		if(generarOferta.getNuevaOfeta() < 0 || generarOferta.getNuevaOfeta() > 1  ){
+			respuesta.setConfirmacion(false);
+			respuesta.setMensaje("el valor de la nueva oferta debe estár en el rango [0.0 , 1.0]");
+			return respuesta;
+		}
+		
+		String tablaProveedor = "";
+		
+		//BÚSQUEDA DE LA TABLA CORRESPONDIENTE AL PROVEEDOR INDICADO POR EL CIF (COLUMNA DBTABLA EN LA TABLA PROVEEDORES)
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+		} catch (Exception e) {
+
+			System.out.println(e.toString());
+		}
+
+		Connection con = null;
+		ResultSet rs = null;
+		Statement cmd = null;
+		try {
+
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3311/proyectoskibd?" + "user=root&password=root");
+			cmd = con.createStatement();
+			
+			//proveedor 1
+			String tabla = "SELECT dbtabla FROM proveedores WHERE cif like '"+generarOferta.getCif()+"';";
+			rs = cmd.executeQuery(tabla);
+			
+			while (rs.next()) {
+				tablaProveedor = rs.getString("dbtabla");;
+			}			
+			cmd = con.createStatement();
+			
+			rs.close();
+
+		} catch (SQLException ex) {
+			System.out.println("ESTÁS EN LA B DEBIDO AL ERROR: SQLException: " + ex.getMessage());
+			respuesta.setConfirmacion(false);
+			respuesta.setMensaje("No existe el CIF en la base de datos");
+			return respuesta;
+		}
+		
+		if(tablaProveedor.equals("")){
+			respuesta.setConfirmacion(false);
+			respuesta.setMensaje("No existe el CIF en la base de datos");
+			return respuesta;
+		}
+		
+		//UPDATE DE LA COLUMNA OFERTA EN LA TABLA CORRESPONDIENTE AL PROVEEDOR INDICADO CON EL CIF
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (Exception e) {
+			System.out.println("ERROR1");
+			e.printStackTrace();
+		}
+		try {
+			Connection conexion;
+			conexion = DriverManager
+					.getConnection("jdbc:mysql://localhost:3311/proyectoskibd?" + "user=root&password=root");
+			Statement s = conexion.createStatement();
+			String consulta = "UPDATE `"+ tablaProveedor +"` SET" + "`oferta` = '"+ generarOferta.getNuevaOfeta() +"' WHERE CIF LIKE '"+ generarOferta.getCif() +"';";
+
+			s.executeUpdate(consulta);
+			respuesta.setConfirmacion(true);
+			respuesta.setMensaje("Se ha generado la oferta correctamente");
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("ESTÁS EN LA B DEBIDO AL ERROR: SQLException: " + e.getMessage());
+			respuesta.setConfirmacion(false);
+			respuesta.setMensaje("No existe el CIF en la base de datos");
+			return respuesta;
+		}
+		
+		return respuesta;
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param leerOfertas
+	 * @return leerOfertasResponse
+	 */
+
+	public org.example.www.gestionproveedores.LeerOfertasResponse leerOfertas(
+			org.example.www.gestionproveedores.LeerOfertas leerOfertas) {
+		LeerOfertasResponse respuesta = new LeerOfertasResponse();
+		
+		//proveedor 1
+		double ski1 = 0;
+		double material1 = 0;
+		double profesor1 = 0; 
+		double alojamiento1 = 0;
+		//proveedor 2
+		double ski2 = 0;
+		double material2 = 0;
+		double profesor2 = 0; 
+		double alojamiento2 = 0;
+		//proveedor 2
+		double ski3 = 0;
+		double material3 = 0;
+		double profesor3 = 0; 
+		double alojamiento3 = 0;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			System.out.println("Registro exitoso");
+
+		} catch (Exception e) {
+
+			System.out.println(e.toString());
+		}
+
+		Connection con = null;
+		ResultSet rs = null;
+		Statement cmd = null;
+		try {
+
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3311/proyectoskibd?" + "user=root&password=root");
+
+			// Otros y operaciones sobre la base de datos...
+
+			cmd = con.createStatement();
+			
+			//proveedor 1
+			String prov = "SELECT * FROM serviciosproveedor";
+			rs = cmd.executeQuery(prov);
+			
+			while (rs.next()) {
+				ski1 = (rs.getInt("ski")* rs.getDouble("oferta"));
+				material1 = (rs.getInt("material")* rs.getDouble("oferta"));
+				profesor1 = (rs.getInt("profesor")* rs.getDouble("oferta"));
+				alojamiento1 = (rs.getInt("alojamiento")* rs.getDouble("oferta"));
+			}			
+			cmd = con.createStatement();
+		
+			//proveedor 2
+			String prov1 = "SELECT * FROM serviciosproveedor1";
+			rs = cmd.executeQuery(prov1);
+			
+			while (rs.next()) {
+				ski2 = (rs.getInt("ski")* rs.getDouble("oferta"));
+				material2 = (rs.getInt("material")* rs.getDouble("oferta"));
+				profesor2 = (rs.getInt("profesor")* rs.getDouble("oferta"));
+				alojamiento2 = (rs.getInt("alojamiento")* rs.getDouble("oferta"));
+			}
+			cmd = con.createStatement();
+			
+			//proveedor 3
+			String prov2 = "SELECT * FROM serviciosproveedor2";
+			rs = cmd.executeQuery(prov2);
+			
+			while (rs.next()) {
+				ski3 = (rs.getInt("ski")* rs.getDouble("oferta"));
+				material3 = (rs.getInt("material")* rs.getDouble("oferta"));
+				profesor3 = (rs.getInt("profesor")* rs.getDouble("oferta"));
+				alojamiento3 = (rs.getInt("alojamiento")* rs.getDouble("oferta"));
+			}			
+			cmd = con.createStatement();
+			
+			rs.close();
+
+		} catch (SQLException ex) {
+
+			// Mantener el control sobre el tipo de error
+			System.out.println("ESTÁS EN LA B DEBIDO AL ERROR: SQLException: " + ex.getMessage());
+
+		}
+		
+		String prov1 = ""+ski1+","+material1+","+profesor1+","+alojamiento1;
+		String prov2 = ""+ski2+","+material2+","+profesor2+","+alojamiento2; 
+		String prov3 = ""+ski3+","+material3+","+profesor3+","+alojamiento3; 
+		
+		respuesta.setMensaje("todo perfe primo, no estás en la B");
+		respuesta.setProv1(prov1);
+		respuesta.setProv2(prov2);
+		respuesta.setProv3(prov3);
+		return respuesta;
+	}
+
+}
